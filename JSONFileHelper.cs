f9f3b0125abe58841b3f6e1a78a57b1f53589945
KunalYednutulwar <121940564+KunalYednutulwar@users.noreply.github.com>
2023-06-07 13:40:31 +0530

    Add files via upload

---
+++ JSONFileHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace APIHelperLibrary
+{
+    public class JSONFileHelper
+    {
+        #region ReadJSONAsAString
+
+        /// <summary>
+        /// This method reads JSON file and returns the content in string form
+        /// </summary>
+        /// <param name="jsonFilepath">Path where JSON file is kept</param>
+        /// <returns>JSON read from file as string</returns>
+        public string ReadJSONAsAString(string jsonFilepath)
+        {
+            try
+            {
+                string json = string.Empty;
+                using (StreamReader r = new StreamReader(jsonFilepath))
+                {
+                    json = r.ReadToEnd();
+                }
+
+                return json;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion ReadJSONAsAString
+
+        #region ReadJSONAsAStringWithEncoding
+
+        /// <summary>
+        ///  This method reads JSON file with enconding type and returns the content in string form
+        /// </summary>
+        /// <param name="jsonFilepath">Path where JSON file is kept</param>
+        /// <param name="encodingType">Encoding Type of JSON text</param>
+        /// <returns>JSON read from file as string</returns>
+        public string ReadJSONAsAString(string jsonFilepath, string encodingType)
+        {
+            try
+            {
+                string json = string.Empty;
+                using (StreamReader r = new StreamReader(jsonFilepath, Encoding.GetEncoding(encodingType)))
+                {
+                    json = r.ReadToEnd();
+                }
+
+                return json;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion ReadJSONAsAStringWithEncoding
+
+        #region ConvertJSONToObject
+
+        /// <summary>
+        /// This method reads JSON file and returns it as an object
+        /// </summary>
+        /// <typeparam name="T">Object type</typeparam>
+        /// <param name="jsonFilepath">JSON file path</param>
+        /// <returns>Generic Object(POCO Class) which is passed as T</returns>
+        public T ConvertJSONToObject<T>(string jsonFilepath)
+        {
+            try
+            {
+                string json = string.Empty;
+                DeserializerHelper deserializerHelper = new DeserializerHelper();
+
+                using (StreamReader r = new StreamReader(jsonFilepath))
+                {
+                    json = r.ReadToEnd();
+                }
+                T deserilisedObj = deserializerHelper.DeserializeJSONToObject<T>(json);
+
+                return deserilisedObj;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion ConvertJSONToObject
+    }
+}
---
+++ XMLFileHelper.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace APIHelperLibrary
+{
+    public class XMLFileHelper
+    {
+        #region ReadXMLAsAString
+
+        /// <summary>
+        /// This method reads XML file and returns the content in string form
+        /// </summary>
+        /// <param name="xmlFilepath">Path where XML file is kept</param>
+        /// <returns>XML read as string from XML file</returns>
+        public string ReadXMLAsAString(string xmlFilepath)
+        {
+            try
+            {
+                string xml = string.Empty;
+                using (StreamReader r = new StreamReader(xmlFilepath))
+                {
+                    xml = r.ReadToEnd();
+                }
+
+                return xml;
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion ReadXMLAsAString
+
+        #region ConvertXMLToObject
+
+        /// <summary>
+        /// This method returns XMl file and returns it as an object
+        /// </summary>
+        /// <typeparam name="T">Object type</typeparam>
+        /// <param name="xmlFilepath">XML file path</param>
+        /// <returns>Generic Object(POCO Class) which is passed as T</returns>
+        public T ConvertXMLToObject<T>(string xmlFilepath)
+        {
+            try
+            {
+                T deserilisedObj;
+                DeserializerHelper deserializerHelper = new DeserializerHelper();
+                using (var stream = new FileStream(xmlFilepath, FileMode.Open))
+                {
+                    deserilisedObj = deserializerHelper.DeserializeXMLToObject<T>(stream);
+                }
+                return deserilisedObj;
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion ConvertXMLToObject
+    }
+}
