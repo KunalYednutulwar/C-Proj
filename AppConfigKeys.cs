8c248a8a6f2e24e36bd548932d2972370b9ee14b
KunalYednutulwar <121940564+KunalYednutulwar@users.noreply.github.com>
2023-10-06 18:13:15 +0530

    Add files via upload

---
+++ AppConfigKeys.cs
@@ -0,0 +1,26 @@
+﻿namespace TestSettings.Settings
+{
+    public class AppConfigKeys
+    {
+        public const string Environment = "Environment";
+        public const string ClientAPIBaseUrl = "ClientAPIBaseUrl";
+        public const string SCClientAPIBaseUrl = "SCClientAPIBaseUrl";
+        public const string Url = "Url";
+        public const string UserName = "UserName";
+        public const string NoRightUserName = "NoRightUserName";
+        public const string TokenAuthenticationEndPoint = "Token_Authentication_End_Point";
+        public const string Password = "Password";
+        public const string ReportPath = "ReportPath";
+        public const string ReportTheme = "ReportTheme";
+        public const string Generic_Settings = "Generic_Settings";
+        public const string AppConfigFileName = "appSettings.json";
+        public const string ResponseTestDataPath = "ResponseTestDataPath";
+        public const string RequestTestDataPath = "RequestTestDataPath";
+        public const string MaxAcceptableResponseTime = "MaxAcceptableResponseTime";
+        public const string PrefixForDataCreation = "PrefixForDataCreation";
+        public const string LogTypeIfResponseTimeExceed = "LogTypeIfResponseTimeExceed";
+        public const string ServerName = "ServerName";
+        public const string ProtocolVersion = "ProtocolVersion";
+        public const string ContentEncoding = "ContentEncoding";
+    }
+}
