f5e45fdfcd58d68223b84ceda0c9f399c7b0ace6
KunalYednutulwar <121940564+KunalYednutulwar@users.noreply.github.com>
2023-10-18 18:29:22 +0530

    Add files via upload

---
+++ HandleScenarioContext.cs
@@ -0,0 +1,42 @@
+﻿using LoggerLibrary;
+using System;
+using System.Reflection;
+using TechTalk.SpecFlow;
+
+namespace TeamNameAPITestScripts
+{
+    internal class HandleScenarioContext
+    {
+        #region Methods for Scenario Context
+
+        /// <summary>
+        /// This method is used to Add or Update ScenarioContext values
+        /// </summary>
+        /// <param name="_scenarioContext">ScenarioContext Type - _scenarioContext</param>
+        /// <param name="key">string value - key</param>
+        /// <param name="value">dynamic - value</param>
+        public static void AddOrUpdateScenarioContextValues(ScenarioContext _scenarioContext, string key, dynamic value)
+        {
+            try
+            {
+                if (_scenarioContext.TryGetValue(key, out dynamic val))
+                {
+                    //Value exists!
+                    _scenarioContext[key] = value;
+                }
+                else
+                {
+                    //Add value
+                    _scenarioContext.Add(key, value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Fail($"Failed Method - {MethodBase.GetCurrentMethod()?.Name} | Exception Message - {ex.Message}");
+                throw;
+            }
+        }
+
+        #endregion Methods for Scenario Context
+    }
+}
